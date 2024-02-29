0ddafc56cba19a0e755975445a54bf109aed411a
puritywaweru7 <puritywaweru48@gmail.com>
2024-02-29 23:26:30 +0300

    Listing Python assignment 2

---
+++ listing.py
@@ -0,0 +1,15 @@
+my_list = []
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
+my_list[1] = 15
+my_list.extend([50,60,70])
+print(my_list)
+my_list.pop()
+print(my_list)
+sorted_list = sorted(my_list)
+print(sorted_list)
+index_of_30 = my_list.index(30)
+print(index_of_30)
